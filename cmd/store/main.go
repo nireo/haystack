3afdefe0a7c7bf242b6f030b05517e6c176474e7
nireo <eemil.lehtikangas1@gmail.com>
2025-05-24 23:38:54 +0300

    added a executable to run the store server

---
+++ cmd/store/main.go
@@ -0,0 +1,23 @@
+package store
+
+import (
+	"flag"
+	"log"
+
+	"github.com/nireo/haystack/store"
+)
+
+func main() {
+	port := flag.Int("port", 8000, "the port on which to host the server")
+	logicalVolumeDir := flag.String("dir", "./logicals", "the path to the logical volume files")
+	flag.Parse()
+
+	st, err := store.NewStore(*logicalVolumeDir)
+	if err != nil {
+		log.Fatalf("cannot create store instance: %s", err)
+	}
+	defer st.Shutdown()
+
+	storeServer := store.NewStoreServer(st, *port)
+	storeServer.Start()
+}
